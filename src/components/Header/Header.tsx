a810d70ef3be665a5343d7fdb187c4c86ba8ffc7
Daljeet singh <daljeetsingh9911@gmail.com>
2023-05-26 16:12:16 +0200

    done with header section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
 
-function Header() {
+import AppBar from '@mui/material/AppBar';
+import {Box,Toolbar,Typography} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import {StyledInputBase,SearchIconWrapper,Search} from './Styles'
+
+export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Travel App
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header
